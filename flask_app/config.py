cb2dfb1b4f18fb85c5a12b1e9c5da574674311b3
rudrank1 <rudrank2004@gmail.com>
2025-04-26 14:30:18 -0400

    hi

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,3 +1,3 @@
 # Stores all configuration values
 SECRET_KEY = 'f5c2f477aa9243f90cc1f1b2f5e5cb7c84ef383e59ae97f9c20690f5f548e519'
-MONGODB_HOST = 'mongodb+srv://pranavdk:pranavdk@cluster0.ckcef49.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0'
+MONGODB_HOST = 'mongodb+srv://atirumal:ITVXGBZzG1MdXUXd@cluster0.pfjli.mongodb.net/atirumal_CMSC388JDB?retryWrites=true&w=majority&appName=Cluster0'
